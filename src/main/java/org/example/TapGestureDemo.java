bf197a586f6264409345370154f41009e20ca1f5
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-08-21 22:43:48 +0530

    Performing Tap Gestures on Mobile Element with Appium

--- src/main/java/org/example/TapGestureDemo.java
+++ src/main/java/org/example/TapGestureDemo.java
@@ -3,9 +3,13 @@ package org.example;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
@@ -22,10 +26,17 @@ public class TapGestureDemo {
             MobileElement productImage = driver.findElement(By.xpath("(//android.view.ViewGroup[@content-desc=\"test-Item\"])[1]/android.view.ViewGroup/android.widget.ImageView"));
             new TouchAction(driver).tap(TapOptions.tapOptions().withElement(ElementOption.element(productImage))).perform();
 
+            ((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.BACK));
+            sleep(2000);
+            Point productImageCoordinates = productImage.getLocation();
+            int productImageXCoordinate = productImageCoordinates.getX();
+            int productImageYCoordinate = productImageCoordinates.getY();
+            new TouchAction(driver).tap(TapOptions.tapOptions().withPosition(PointOption.point(productImageXCoordinate, productImageYCoordinate))).perform();
+
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         } finally {
-            sleep(3000);
+            sleep(2000);
             driver.quit();
         }
 

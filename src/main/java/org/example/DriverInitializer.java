0d2040a95740b15e27835412b376596e0d82d243
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-08-21 14:05:19 +0530

    Created a class for Initializing the driver

---
+++ src/main/java/org/example/DriverInitializer.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class DriverInitializer {
+    public void initializeDriver(){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Oppo_A76");
+        caps.setCapability("udid", "emulator-5554"); // You can get it from 'adb devices' command
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("app", "/Users/testvagrant/Downloads/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk"); // Replace with your app's path
+        caps.setCapability("appPackage", "com.swaglabsmobileapp");
+        caps.setCapability("appActivity", "com.swaglabsmobileapp.MainActivity");
+    }
+}
